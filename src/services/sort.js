4bc9d9afb4a63f790672514a7be2d15b631da70c
adewunmi <shonibareadewunmicomfort@gmail.com>
2021-07-24 00:53:45 +0100

    add edit and delete functionality

--- src/services/sort.js
+++ src/services/sort.js
@@ -1,4 +1,4 @@
-import { saveToStorage } from './update';
+import { saveToStorage, getTodos } from './update';
 
 export const handleDragStart = (evt) => {
   evt.dataTransfer.setData('text/plain', evt.target.dataset.index);
@@ -19,8 +19,9 @@ export const cancelDefault = (e) => {
   return false;
 };
 
-export const handleDrop = (e, todos) => {
+export const handleDrop = (e) => {
   cancelDefault(e);
+  const todos = getTodos();
 
   // get new and old index
   const oldIndex = e.dataTransfer.getData('text/plain');
